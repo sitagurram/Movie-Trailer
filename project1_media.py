f599aa85ca7c4da8bdb04772a12c62ed7ea8f13f
sita sowjanya gurram <sita.namburi@gmail.com>
2016-11-10 15:06:26 -0500

    Update project1_media.py

--- project1_media.py
+++ project1_media.py
@@ -1,8 +1,11 @@
 import webbrowser
 
+
 class Python():
-       
-    def __init__(self,movie_title,poster_image,trailer_youtube):
-        self.title=movie_title
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer_youtube
+    """This application will display movie posters,We can play trailer by
+    clicking it"""
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
